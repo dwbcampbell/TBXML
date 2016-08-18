b2b958d33b2ea7b324fa42a8d6fa88c0606f5639
Douglas Campbell <dwbcampbell@gmail.com>
2016-08-18 19:54:48 -0400

    rename shared library

---
+++ TBXML/TBXML.h
@@ -0,0 +1,19 @@
+//
+//  TBXML.h
+//  TBXML
+//
+//  Created by Douglas Campbell on 2016-08-18.
+//  Copyright © 2016 71 Squared. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TBXML.
+FOUNDATION_EXPORT double TBXMLVersionNumber;
+
+//! Project version string for TBXML.
+FOUNDATION_EXPORT const unsigned char TBXMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TBXML/PublicHeader.h>
+
+
